af9ad7edb561bf0ddd7ca76922100f1968edde4c
youqiaohaozi <youqiaohaozi@126.com>
2020-11-03 11:55:35 +0800

    update case

--- testcases/test_cacu.py
+++ testcases/test_cacu.py
@@ -4,21 +4,29 @@ from pythoncode.caculator import Calculator
 
 
 class TestCalc:
+    calc = Calculator()
     @pytest.mark.parametrize('a,b,expect',
                              [[1, 1, 2], [10, 10, 15], [-1, -2, 3], [-1, 2, 1], [0.5, 0.7, 1], [0.2, 0.3, 0.5]],
                              ids=["positives", "positive1", "nagetives", "positive+nagetive", "floats", "floats1"])
     def test_add(self, a, b, expect):
-        calc = Calculator()
-        result = calc.add(a, b)
+        # calc = Calculator()
+        result = self.calc.add(a, b)
         assert result == expect
+
+    @pytest.mark.parametrize('a,b,expect',
+                             [[0.5, 0.7, 1], [0.2, 0.3, 0.5]],
+                             ids=["floats", "floats1"])
+    def test_add_float(self, a, b, expect):
+        result = self.calc.add(a, b)
+        assert round(result, 2) == expect
+
 
     @pytest.mark.parametrize('a,b,expect',
                              [[1, 1, 1], [10, -2, -5], [-2, -1, 2], [3, 2, 1.5], [0.7, 0.2, 3.5], [0.3, 0.15, 2]],
                              ids=["positives", "pos/nage", "nagetives", "expectfloat", "floats", "expectpositive"])
     def test_div(self, a, b, expect):
-        calc = Calculator()
-        result = calc.div(a, b)
-        assert result == expect
+        result = self.calc.div(a, b)
+        assert round(result, 2) == expect
 
     def setup_class(self):
         print("开始验证计算结果正确性")
@@ -34,4 +42,4 @@ class TestCalc:
 
 
 if __name__ == '__main__':
-    pytest.main(['-s -v', 'test_cacu.py'])
+    pytest.main(['-s', '-v', 'test_cacu.py'])
