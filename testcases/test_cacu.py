0f595c8fe15ec98cab3e99ec3696f7f50830f887
youqiaohaozi <youqiaohaozi@126.com>
2020-11-03 14:00:08 +0800

    update case

--- testcases/test_cacu.py
+++ testcases/test_cacu.py
@@ -4,7 +4,6 @@ from pythoncode.caculator import Calculator
 
 
 class TestCalc:
-    calc = Calculator()
     @pytest.mark.parametrize('a,b,expect',
                              [[1, 1, 2], [10, 10, 15], [-1, -2, 3], [-1, 2, 1], [0.5, 0.7, 1], [0.2, 0.3, 0.5]],
                              ids=["positives", "positive1", "nagetives", "positive+nagetive", "floats", "floats1"])
@@ -28,8 +27,14 @@ class TestCalc:
         result = self.calc.div(a, b)
         assert round(result, 2) == expect
 
+    @pytest.mark.parametrize('a,b', [[0.1, 0], [10, 0], [1, 1]])
+    def test_div_zero(self, a, b):
+        with pytest.raises(ZeroDivisionError):
+            self.calc.div(a, b)
+
     def setup_class(self):
         print("开始验证计算结果正确性")
+        self.calc = Calculator()
 
     def teardown_class(self):
         print("全部计算结果验证完成")
